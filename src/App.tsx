b87ab32bcbcbb6e34d823608cfaa86bf9a9078de
Alexander <alxtim10@gmail.com>
2025-04-20 21:32:53 +0700

    integrate engine dropdown and chosen engine api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css';
-import Home from './pages/home/Home';
+import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <main>
-      <h1 className="fixed border-b border-b-[#313131] w-full font-semibold h-[60px] flex items-center justify-center text-xl bg-[#212121]">FitGPT</h1>
       <Home />
     </main>
   );
